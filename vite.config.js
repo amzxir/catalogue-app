8dfb784ad8e391a9604d088b3145db22d33b3bdc
amzxir <amirreza79ahmadi@gmail.com>
2024-01-23 21:53:14 +0330

    fixed cors

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://react-mini-projects-api.classbon.com',
+      },
+    },
+  },
   plugins: [react()],
   base: "/catalogue-app/"
 })
