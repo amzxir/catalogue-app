217fe4dd2abf24039c9864b119131812ad7f702f
amzxir <amirreza79ahmadi@gmail.com>
2024-01-23 14:11:33 +0330

    tests

--- src/Components/CategoryList/categoryList.jsx
+++ src/Components/CategoryList/categoryList.jsx
@@ -15,7 +15,6 @@ const CategoryList = ({ filterProduct, children }) => {
                 headers: {
                     "Content-Type": "application/json",
                     "Access-Control-Allow-Origin": "https://amzxir.github.io",
-                    "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
                     "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length"
                 }, 
             }
